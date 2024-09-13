d2107606436f55029771846e6a685ef8c19aa020
vignesh B <145856381+Vignesh12111@users.noreply.github.com>
2024-09-13 13:18:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Carts from './Components/Carts'
+import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom'
+import ProductContextComponents from './utils/ProductContextComponents'
+
+function App() {
+  return <>
+    <BrowserRouter>
+    <Routes>
+    <Route path='' element={
+        <ProductContextComponents>
+          <Carts/>
+        </ProductContextComponents>}>
+        <Route path='*' element={<Navigate to='/carts'/>}/>
+        </Route>
+    </Routes>
+    </BrowserRouter>
+   
+    
+  </>
+}
+
+export default App
